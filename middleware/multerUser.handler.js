517dfe265dfd69dfe975b8b43c12d75b0c34b08f
cristianmflorez <cristianmflorez@gmail.com>
2023-03-10 17:22:10 -0500

    frontend deployed 2

--- middleware/multerUser.handler.js
+++ middleware/multerUser.handler.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, '/');
+		cb(null, process.env.BUCKET);
 	},
 	filename: function (req, file, cb) {
 		cb(null, `${file.fieldname}_${Date.now()}_${path.extname(file.originalname)}`);
