ed84e96bdbc37bdd5ed09f20c76d138769394a32
cristianmflorez <cristianmflorez@gmail.com>
2023-03-13 20:16:42 -0500

    railway

--- middleware/multerUser.handler.js
+++ middleware/multerUser.handler.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		fs.mkdir('/tmp/users',(err)=>{
-			cb(null, '/tmp/users');
+		fs.mkdir('/users',(err)=>{
+			cb(null, '/users');
 		 });
 	},
 	filename: function (req, file, cb) {
